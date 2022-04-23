36047d39d50b7462f8c485c569d7f2a9a1035a3f
Massimiliano Gerardi <massi@ambulando.net>
2022-04-23 20:15:06 +0200

    move file server files to src

--- file-server/services/files.ts
+++
@@ -1,9 +0,0 @@
-import * as Buffer from "buffer";
-import MD5 from "crypto-js/md5";
-export const decryptFile = async (encryptedFile: Buffer, key: string): Promise<Buffer> => {
-  return encryptedFile;
-}
-
-export const generateUrl = async (path: string, passphrase: string): Promise<string> => {
-  return `${path}?key=${MD5(passphrase).toString()}`
-}
